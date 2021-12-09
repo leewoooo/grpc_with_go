e3af3f526777fa520437c65172427490360e5b2a
leewoooo <leecoding2285@gmail.com>
2021-12-10 00:17:00 +0900

    ✨Add: chap_06 client unray interceptor ex

--- chap_05/internal/interceptor/unray/interceptor.go
+++ chap_05/internal/interceptor/unray/interceptor.go
@@ -7,8 +7,9 @@ import (
 	"google.golang.org/grpc"
 )
 
-// UnrayOrderManagementInterceptorfunc unray interceptor
-func UnrayOrderManagementInterceptorfunc(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+// UnrayOrderManagementServerInterceptor unray interceptor
+// https://github.com/grpc/grpc-go/blob/master/examples/features/interceptor/README.md#unary-interceptor-1
+func UnrayOrderManagementServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 	// 전처리
 	logrus.WithContext(ctx).
 		Infof("==== [Server Interceptor] method: %s", info.FullMethod)
@@ -21,4 +22,19 @@ func UnrayOrderManagementInterceptorfunc(ctx context.Context, req interface{}, i
 		Infof("Post Proc Message: %+v", resp)
 
 	return resp, err
+}
+
+// UnrayOrderManagementClientInterceptor unray client interceptor
+// https://github.com/grpc/grpc-go/blob/master/examples/features/interceptor/README.md#unary-interceptor
+func UnrayOrderManagementClientInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	logrus.WithContext(ctx).Infof("==== client unray interceptor method: %s req: %T ====", method, req)
+
+	err := invoker(ctx, method, req, reply, cc, opts...)
+
+	if err != nil {
+		logrus.WithContext(ctx).WithError(err).Errorf("failed Reqeust RPC met")
+		return err
+	}
+
+	return nil
 }
